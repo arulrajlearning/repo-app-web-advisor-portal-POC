c270b22edffb59a20884919e49f9dd0b740c7429
arulrajlearning <kavithajoycearulraj@gmail.com>
2025-06-26 11:20:34 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,55 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
-import { useAuthenticator } from '@aws-amplify/ui-react';
+import { useAuthenticator } from '@aws-amplify/ui-react-core';
 import { generateClient } from "aws-amplify/data";
+import type { AuthUser } from '@aws-amplify/auth';
+import { fetchAuthSession } from 'aws-amplify/auth';
 
 const client = generateClient<Schema>();
 
 function App() {
+  
   const {signOut} = useAuthenticator();
+  const { user } = useAuthenticator((context) => [context.user]);
+
+  const typedUser: AuthUser = user;
+  const [idToken, setIdToken] = useState<string | null>(null);
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  useEffect(() => {
+    async function getToken() {
+      try {
+        const session = await fetchAuthSession();
+        const token = session.tokens?.idToken?.toString();
+        setIdToken(token ?? 'No ID token found');
+      } catch (error) {
+        console.error('Error fetching session:', error);
+        setIdToken('Error fetching token');
+      }
+    }
+
+    getToken();
+  }, []);
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
+  }, []);
+
+  useEffect(() => {
+    async function getToken() {
+      try {
+        const session = await fetchAuthSession();
+        const token = session.tokens?.idToken?.toString();
+        setIdToken(token ?? 'No ID token found');
+      } catch (error) {
+        console.error('Error fetching session:', error);
+        setIdToken('Error fetching token');
+      }
+    }
+
+    getToken();
   }, []);
 
   function createTodo() {
@@ -35,6 +72,11 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+      <div>
+      <p><strong>Username:</strong> {typedUser.username}</p>
+      <p><strong>User ID:</strong> {typedUser.userId}</p>
+      <p><strong>Email:</strong> {typedUser.signInDetails?.loginId}</p>
+    </div>
       <button onClick={signOut}>Sign out</button>
     </main>
   );
