60c1cbae81cd06e28dac44c96eba2fdde4458e5a
aponnudurai-cfn <164194534+aponnudurai-cfn@users.noreply.github.com>
2025-07-02 09:46:33 -0400

    Update getProfile.ts

--- amplify/functions/api-function/getProfile.ts
+++ amplify/functions/api-function/getProfile.ts
@@ -1,6 +1,21 @@
 import type {APIGatewayProxyHandler} from "aws-lambda";
 export const handler: APIGatewayProxyHandler = async (event) => {
   console.log("event", event);
+  const claims = event.requestContext?.authorizer?.claims;
+  const email = claims?.email || "unknown";
+
+  const now = new Date();
+  const hour = now.getHours();
+
+   let greeting = '';
+  if (hour >= 5 && hour < 12) {
+    greeting = "Good morning";
+  } else if (hour >= 12 && hour < 17) {
+    greeting = "Good afternoon";
+  } else {
+    greeting = "Good evening";
+  }
+
   return {
     statusCode: 200,
     // Modify the CORS settings below to match your specific requirements
@@ -8,6 +23,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       "Access-Control-Allow-Origin": "*", // Restrict this to domains you trust
       "Access-Control-Allow-Headers": "*", // Specify only the headers you need to allow
     },
-    body: JSON.stringify("Hello from getProfile[ById][ByEmailAddress] - arulraj joseph!"),
+    /*body: JSON.stringify("Hello from getProfile[ById][ByEmailAddress] - arulraj joseph!"),*/
+    body: JSON.stringify(`${greeting}, ${email}!`),
   };
 };
