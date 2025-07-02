c92675c724f3dc3a2b5f6a2c7b73f206550e8ef2
aponnudurai-cfn <164194534+aponnudurai-cfn@users.noreply.github.com>
2025-07-02 12:53:36 -0400

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,107 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { Stack } from "aws-cdk-lib";
+import {
+  AuthorizationType,
+  CognitoUserPoolsAuthorizer,
+  Cors,
+  LambdaIntegration,
+  RestApi,
+} from "aws-cdk-lib/aws-apigateway";
+import { Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { getPeople, getProfile } from "./functions/api-function/resource";
+import { auth } from "./auth/resource";
+import { data } from "./data/resource";
+import { EndpointType } from "aws-cdk-lib/aws-apigateway";
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
+  getPeople,
+  getProfile
+});
+
+// create a new API stack
+const apiStack = backend.createStack("advisor-api-stack");
+
+const cognitoAuth = new CognitoUserPoolsAuthorizer(apiStack, "CognitoAuth", {
+  cognitoUserPools: [backend.auth.resources.userPool],
+});
+
+
+// create a new REST API
+const advisorPortalApi = new RestApi(apiStack, "advisor-portal-api", {
+  restApiName: "advisor-portal-api",
+  deploy: true,
+  deployOptions: {
+    stageName: "dev", 
+  },
+  defaultCorsPreflightOptions: {
+    allowOrigins: Cors.ALL_ORIGINS, // use specific domain(s) in production
+    allowMethods: Cors.ALL_METHODS, // or [ 'GET', 'POST', 'OPTIONS' ]
+    allowHeaders: Cors.DEFAULT_HEADERS,
+  },
+  /*endpointConfiguration: {
+    types: [EndpointType.REGIONAL],
+  }*/
+});
+
+// create a new Lambda integration
+const getPeopleIntegration = new LambdaIntegration(
+  backend.getPeople.resources.lambda
+);
+const getProfileIntegration = new LambdaIntegration(
+  backend.getProfile.resources.lambda
+);
+
+// create a new resource path with IAM authorization
+const people = advisorPortalApi.root.addResource("People", {
+  defaultMethodOptions: {
+    authorizationType: AuthorizationType.COGNITO,
+    authorizer: cognitoAuth,
+  },
+});
+people.addMethod("GET", getPeopleIntegration); 
+
+const profile = advisorPortalApi.root.addResource("Profile", {
+  defaultMethodOptions: {
+    authorizationType: AuthorizationType.COGNITO,
+    authorizer: cognitoAuth,
+  },
+});
+profile.addMethod("GET", getProfileIntegration); 
+
+// create a new IAM policy to allow Invoke access to the API
+const advisorApiPolicy = new Policy(apiStack, "AdvisorApiPolicy", {
+  statements: [
+    new PolicyStatement({
+      actions: ["execute-api:Invoke"],
+      resources: [
+        `${advisorPortalApi.arnForExecuteApi("*", "/People", "dev")}`,
+        `${advisorPortalApi.arnForExecuteApi("*", "/People/*", "dev")}`,
+        `${advisorPortalApi.arnForExecuteApi("*", "/Profile", "dev")}`,
+        `${advisorPortalApi.arnForExecuteApi("*", "/Profile/*", "dev")}`,
+      ],
+    }),
+  ],
+});
+
+// attach the policy to the authenticated and unauthenticated IAM roles
+backend.auth.resources.authenticatedUserIamRole.attachInlinePolicy(
+  advisorApiPolicy
+);
+backend.auth.resources.unauthenticatedUserIamRole.attachInlinePolicy(
+  advisorApiPolicy
+);
+
+// add outputs to the configuration file
+backend.addOutput({
+  custom: {
+    API: {
+      [advisorPortalApi.restApiName]: {
+        endpoint: advisorPortalApi.url,
+        region: Stack.of(advisorPortalApi).region,
+        apiName: advisorPortalApi.restApiName,
+      },
+    },
+  },
 });
