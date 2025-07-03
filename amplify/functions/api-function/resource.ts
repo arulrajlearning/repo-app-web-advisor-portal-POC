fd90e01deef49e9d6370b335682e1c22d9966368
arulrajlearning <kavithajoycearulraj@gmail.com>
2025-07-03 07:12:33 -0400

    Update resource.ts

--- amplify/functions/api-function/resource.ts
+++ amplify/functions/api-function/resource.ts
@@ -3,9 +3,9 @@ export const userProfile = defineFunction({
   name: "UserProfile",
   entry: "./UserProfile.ts", 
   environment: {
-    Location_API_URL: process.env['Location_API_URL']!,
-    Weather_API_URL: process.env['Weather_API_URL']!,
-    WEATHER_API_KEY : process.env['WEATHER_API_KEY']!, // Replace with your actual API key
+    Location_API_URL: process.env['Location_API_URL'] ?? '',
+    Weather_API_URL: process.env['Weather_API_URL'] ?? '',
+    WEATHER_API_KEY : process.env['WEATHER_API_KEY'] ?? '', // Replace with your actual API key
   }
 }); 
 export const userPersonalization = defineFunction({
