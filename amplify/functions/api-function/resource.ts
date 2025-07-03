5226040018928153a90d95dff55c6606a4f790fd
arulrajlearning <kavithajoycearulraj@gmail.com>
2025-07-03 08:27:01 -0400

    secret change

--- amplify/functions/api-function/resource.ts
+++ amplify/functions/api-function/resource.ts
@@ -1,11 +1,11 @@
-import { defineFunction} from "@aws-amplify/backend";
+import { defineFunction, secret} from "@aws-amplify/backend";
 export const userProfile = defineFunction({
   name: "UserProfile",
   entry: "./UserProfile.ts", 
   environment: {
-    Location_API_URL: process.env['Location_API_URL'] ?? '',
-    Weather_API_URL: process.env['Weather_API_URL'] ?? '',
-    WEATHER_API_KEY : process.env['WEATHER_API_KEY'] ?? '', // Replace with your actual API key
+    Location_API_URL: secret('Location_API_URL') ?? '',
+    Weather_API_URL: secret('Weather_API_URL')?? '',
+    WEATHER_API_KEY : secret('WEATHER_API_KEY') ?? '', // Replace with your actual API key
   }
 }); 
 export const userPersonalization = defineFunction({
